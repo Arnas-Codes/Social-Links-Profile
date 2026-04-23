8da86097728ae55a4ffcb3b626a51b9086563ce2
Arnas Foysal <arnas.codes@gmail.com>
2026-04-23 23:32:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import SocialCard from "./Social_card.jsx"
+
+function App() {
+
+  return (
+    <div className="bg-Grey-900 py-24 h-screen text-white font-inter">
+    <SocialCard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Social_card.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const SocialCard = () => {
+  return (
+    <div className="flex flex-col items-center text-center bg-Grey-800 w-xs m-auto py-6 gap-6 rounded-xl">
+      <img
+        className="w-16 rounded-full"
+        src="src/assets/avatar-jessica.jpeg"
+        alt="Profile_img"
+      />
+      <div>
+        <h1 className="text-2xl">Jessica Randall</h1>
+        <p className="text-xs text-Green mt-1">London, United Kingdom </p>
+      </div>
+      <p className="text-xs">"Front-end developer and avid reader."</p>
+      <div className="flex flex-col gap-3.5 text-xs w-[80%] font-semibold ">
+        <button className="bg-Grey-700 py-3 rounded-md hover:bg-Green hover:text-black cursor-pointer">
+          GitHub
+        </button>
+        <button className="bg-Grey-700 py-3 rounded-md hover:bg-Green hover:text-black cursor-pointer">
+          Frontend Mentor
+        </button>
+        <button className="bg-Grey-700 py-3 rounded-md hover:bg-Green hover:text-black cursor-pointer">
+          LinkedIn
+        </button>
+        <button className="bg-Grey-700 py-3 rounded-md hover:bg-Green hover:text-black cursor-pointer">
+          Twitter
+        </button>
+        <button className="bg-Grey-700 py-3 rounded-md hover:bg-Green hover:text-black cursor-pointer">
+          Instagram
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SocialCard;
